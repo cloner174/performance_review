ced8d60c975011a26c83dfe159ad8eb13c7713bc
cloner174 <hamedhajipour1400@gmail.com>
2024-11-12 02:10:58 +0330

    add urls

---
+++ evaluations/urls.py
@@ -0,0 +1,13 @@
+#
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('employees/', views.employee_list, name='employee_list'),
+    path('employees/create/', views.create_employee, name='create_employee'),
+    path('employees/<int:employee_id>/evaluate/', views.evaluate_employee, name='evaluate_employee'),
+    path('evaluations/<int:evaluation_id>/', views.view_evaluation, name='view_evaluation'),
+    path('evaluations/<int:evaluation_id>/export/', views.export_evaluation_to_excel, name='export_evaluation_to_excel'),
+    path('my-evaluations/', views.view_own_evaluations, name='view_own_evaluations'),
+]
+#cloner174
